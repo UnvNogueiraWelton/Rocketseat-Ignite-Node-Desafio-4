05c4b38c097bf204d72449eb8f8e901ac1bb9b36
Welton Nogueira <welton.nogueira.dev@gmail.com>
2022-06-06 22:02:19 -0300

    Add Docs

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerui from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerui.serve, swaggerui.setup(swaggerFile));
 app.use("/users", usersRoutes);
 
 export { app };
